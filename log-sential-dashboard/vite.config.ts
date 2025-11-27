0357dc8792e2af3032d042c6e834ad3799e6fd51
Krish <krishprajapati1248@gmail.com>
2025-11-27 10:17:08 +0530

    changes in vite file for magic link

--- log-sential-dashboard/vite.config.ts
+++ log-sential-dashboard/vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; connect-src 'self' https://*.supabase.co"
+    }
+  }
 })
